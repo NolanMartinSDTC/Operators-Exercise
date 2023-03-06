26d8e1fabf76374a32ffa56c395e0cdf046d85d6
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-06 11:02:46 -0700

    added code for operators in ex1, calculated area of circle in ex 2 without using method

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+
+            int a = 17;
+            int b = 4;
+
+            //Console.WriteLine($"Addition: {a+b}");
+
+            //Console.WriteLine($"Subtraction: {a-b}");
+
+            //Console.WriteLine($"Multiplication: {a*b}");
+            
+            int quotient = a / b;
+            int remainder = a % b;
+
+            //Console.WriteLine($"Division: {quotient}");
+            //Console.WriteLine($"Modulus: {remainder}");
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+            //Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse((Console.ReadLine()));
+            double AreaOfCircle = Math.PI*Math.Pow(radius,2);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {AreaOfCircle}");
         }
     }
 }
